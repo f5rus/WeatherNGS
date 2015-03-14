b4a593818cf8aaf6818b54fa7658aabac5679ae6
Vit <millionaire@mail.ru>
2015-03-14 23:46:28 +0600

    cron path

--- cron/request.php
+++ cron/request.php
@@ -1,12 +1,13 @@
-<?php
+﻿<?php
 /**
  * скрипт который должен запускаться с периодом времени для парсинга информации
  * о текущей погоде и записи ее в базу данных 
  */
-require 'classes/DB.class.php';
-require 'classes/PogodaNGS.class.php';
-require 'classes/ForecastDocument.class.php';
-require 'classes/Forecast.class.php';
+require '../classes/Cities.class.php';
+require '../classes/DB.class.php';
+require '../classes/PogodaNGS.class.php';
+require '../classes/ForecastDocument.class.php';
+require '../classes/Forecast.class.php';
 try {
     DB::init();
     $response = Forecast::request();
